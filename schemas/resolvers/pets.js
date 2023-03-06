dff12939d091a428c5508d9a550d14e8cc65781d
Grayson Harvey <graycodes.nu@gmail.com>
2023-03-06 15:54:38 -0600

    added pet query and mutations

--- schemas/resolvers/pets.js
+++ schemas/resolvers/pets.js
@@ -1,1 +1,39 @@
 // TODO: Routes
+const Pet = require ("../../models/Pet");
+
+module.exports = {
+
+  Query: {
+    pet: async (parent, {petId}) => {
+      return Pet.findOne({ _id: petId });
+    },
+
+
+  }, 
+
+  Mutation: {
+    addPet: async (parent, {user}, context) => {
+      if(context.user) {
+        console.log('Adding pet to user profile')
+        return User.FindOneAndUpdate(
+          { _id: context.user._id },
+          {
+            petList: petList
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+    },
+    removePet: async (parent, {userId, petId}) => {
+      return User.FindOneAndUpdate(
+        { _id: userId },
+        { $pull: {petList: { _id: {$eq: petId} } } },
+        { new: true }
+      );
+    },
+  }
+
+}
