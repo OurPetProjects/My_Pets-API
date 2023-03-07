71b1de5268099403b091a35d52608ef10c126f24
Grayson Harvey <graycodes.nu@gmail.com>
2023-03-06 21:09:40 -0600

    tasks!! seed data setup, resolvers start

--- schemas/resolvers/tasks.js
+++ schemas/resolvers/tasks.js
@@ -3,13 +3,19 @@ const Task = require("../../models/Task");
 
 module.exports = {
   Mutation: {
-    // Add task to pet via pet id, which is under user id
+    // TODO: Add task to pet via pet id, which is under user id
     addTask: async (parent, { petId, taskId }) => {
       return Pet.FindOneAndUpdate(
         { _id: petId },
-        { $pull: { taskList: { _id: { $eq: taskId } } } },
+        { $push: { taskList: { _id: { $eq: taskId } } } },
         { new: true }
       );
     },
+
+    // TODO: Edit task
+
+    // TODO: Delete task
+
+
   },
 };
