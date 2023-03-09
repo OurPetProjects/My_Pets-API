342e325ee0243bf218b98e87ecee511874b0a80a
Grayson Harvey <graycodes.nu@gmail.com>
2023-03-08 21:01:48 -0600

    updated task seeds, models to seed database. database seed working!

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,17 @@
 const db = require('../config/connection');
 const { User, Pet } = require('../models');
-// const userSeed = require('./userSeed.json');
+const userSeed = require('./userSeed.json');
 const petSeed = require('./petSeed.json');
+
 
 db.once('open', async () => {
   try {
-    // await User.deleteMany({});
-    // await User.create(userSeed)
+    await User.deleteMany({});
+    await User.create(userSeed)
 
     await Pet.deleteMany({});
-    await Pet.create(petSeed)
+    await Pet.create(petSeed);
+
 
     console.log('all done!');
     process.exit(0);
