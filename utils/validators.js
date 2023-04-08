26772c84ab7bf653b708b429c3c93f14a084446d
Eric McGhee <eric.mcghee@northwestern.edu>
2023-04-08 01:12:38 -0500

    Re-added email validator and excluded double @@ for email validator

--- utils/validators.js
+++ utils/validators.js
@@ -9,22 +9,22 @@ module.exports.validateRegisterInput = (
         errors.username = "Invalid";
     }
     if (email.trim() === "") {
-        errors.email = "Invalid";
+        errors.email = "Invalid - No email added";
     }
     // else {
     //     const regEx =
-    //         // /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-    //         /.+@.+\..+/;
+    //         /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    //         // /.+@.+\..+/;
     //     if (!email.match(regEx)) {
-    //         errors.email = "Invalid poodle";
+    //         errors.email = "Invalid - bad email address";
     //     }
     // }
     if (password === "") {
-        errors.password = "Invalid";
+        errors.password = "Invalid - Please type a password";
     // } else if (password !== confirmPassword) {
     //     errors.confirmPassword = "Invalid";
     } else if (password.length < 4) {
-        errors.password = "Invalid";
+        errors.password = "Invalid - Password length needs to be more than 4 characters";
     }
     return {
         errors,
